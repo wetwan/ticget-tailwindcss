0ca51f4f857d81fccec70c93a05da918abbe769b
wetwan <157417484+wetwan@users.noreply.github.com>
2024-08-03 13:30:07 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/**/*.{html,js}"],
+  theme: {
+    extend: {}, 
+    container:{
+      center: true
+    },
+  },
+  plugins: [],
+}
